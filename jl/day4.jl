ee57046fd54b99e8ed92515b669bfeeb9037ca28
Eric Ekholm <eric.ekholm@gmail.com>
2022-12-05 12:07:39 -0500

    --amend

--- jl/day4.jl
+++ jl/day4.jl
@@ -21,3 +21,14 @@ res1 = sum(is_overlap.(z))
 
 # Part 2
 
+function is_any_overlap(x)
+    if (
+        (x[2] >= x[3] && x[1] <= x[3]) |
+        (x[4] >= x[1] && x[3] <= x[2])
+    )
+        return true
+    else return false
+    end
+end
+
+res2 = sum(is_any_overlap.(z))
